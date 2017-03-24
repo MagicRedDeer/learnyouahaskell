47294c6d44b6e58285ec23b31c229997c62592c0
Talha Ahmed <talha.ahmed@gmail.com>
2017-03-24 21:28:46 +0500

    starting out

---
+++ startingout.hs
@@ -0,0 +1,5 @@
+
+{-Problem: which right triangle that has integers for all sides and all sides equal to or smaller than 10 has a perimeter of 24? -}
+
+triangles = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10]]
+rightTriangles = [(a, b, c) | c <- [1..10],  b <- [1..c], a <- [1..b], c^2==a^2+b^2, a+b+c==24]
